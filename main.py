298c1734af3d695cef5a61fdec5a4ea680b75c02
The-Cuban-Is-In <101074377+The-Cuban-Is-In@users.noreply.github.com>
2022-08-23 13:39:11 -0400

    Minor Updates
    - Updated to add icons to main/edit
      menus
    - Updated to hid home options when in
      edit options and vice versa.

--- main.py
+++ main.py
@@ -22,13 +22,18 @@ class ImageEditor(qtw.QWidget):
         self.setGeometry(500, 50, 500, 800)
         self.setWindowTitle('Alter')
         self.setStyleSheet("""
-            background: white;
+            QWidget {background: black;}
+            QPushButton {
+                        color: white;
+                        background-color: rgba(0, 0, 0, 0)
+                        }
             """)
 
     def init_window_ui(self):
         ##################TEMP##################
         ##################TEMP##################
-        self.img = Image.open(self.current_img[self.current_img_index])
+        self.origional_img = Image.open(self.current_img[self.current_img_index])
+        self.img = self.origional_img
         self.pixmap = ImageQt.toqpixmap(self.img)
         self.scaled_img = self.pixmap.scaled(500, 800, qtc.Qt.KeepAspectRatio)
         ##################TEMP##################
@@ -47,16 +52,19 @@ class ImageEditor(qtw.QWidget):
             """)
 
         # Sets up main buttons
-        self.rotate_img_right = qtw.QPushButton('90R', clicked = lambda: self.rotate_img('R'))
-        self.rotate_img_left = qtw.QPushButton('90L', clicked = lambda: self.rotate_img('L'))
-        self.delete_img = qtw.QPushButton('Del', clicked = self.delete_current_img)
-        self.edit_img = qtw.QPushButton('Edit', clicked = self.edit_options)
+        self.rotate_img_right = qtw.QPushButton(clicked = lambda: self.rotate_img('R'))
+        self.rotate_img_right.setIcon(qtg.QIcon(os.path.join('icons', 'rotate-right.png'))) 
+
+        self.rotate_img_left = qtw.QPushButton(clicked = lambda: self.rotate_img('L'))
+        self.rotate_img_left.setIcon(qtg.QIcon(os.path.join('icons', 'rotate-left.png')))
+
+        self.edit_img = qtw.QPushButton(clicked = self.edit_options)
+        self.edit_img.setIcon(qtg.QIcon(os.path.join('icons', 'edit-icon.png')))
 
         # Sets widgets to layouts
         self.row_one_layout.addWidget(self.main_pic_label)
-        self.row_two_layout.addWidget(self.delete_img)
-        self.row_two_layout.addWidget(self.edit_img)
         self.row_two_layout.addWidget(self.rotate_img_left)
+        self.row_two_layout.addWidget(self.edit_img)
         self.row_two_layout.addWidget(self.rotate_img_right)
         
 
@@ -76,10 +84,47 @@ class ImageEditor(qtw.QWidget):
         self.scaled_img = self.pixmap.scaled(500, 800, qtc.Qt.KeepAspectRatio)
         self.main_pic_label.setPixmap(self.scaled_img)
 
-    def edit_options(self):
+    def edit_options(self): # TODO: find a way to hide main widgets without doing it individually
+        self.rotate_img_left.hide()
+        self.rotate_img_right.hide()
+        self.edit_img.hide()
+
+        # create new buttons (back, brighten, crop, grey scale)
+        self.back_btn = qtw.QPushButton(clicked = self.return_home)
+        self.back_btn.setIcon(qtg.QIcon(os.path.join('icons', 'back-icon.png')))
+
+        self.brightness_btn = qtw.QPushButton(clicked = self.brighten_options)
+        self.brightness_btn.setIcon(qtg.QIcon(os.path.join('icons', 'brightness-icon.png')))
+
+        self.crop_btn = qtw.QPushButton()
+        self.crop_btn.setIcon(qtg.QIcon(os.path.join('icons', 'crop-icon.png')))
+
+        self.filter_btn = qtw.QPushButton()
+        self.filter_btn.setIcon(qtg.QIcon(os.path.join('icons', 'filter-icon.png')))
+
+        # add new buttons to layout
+        self.row_two_layout.addWidget(self.back_btn)
+        self.row_two_layout.addWidget(self.brightness_btn)
+        self.row_two_layout.addWidget(self.crop_btn)
+        self.row_two_layout.addWidget(self.filter_btn)
+
+    def return_home(self):
+        self.back_btn.hide()
+        self.brightness_btn.hide()
+        self.crop_btn.hide()
+        self.filter_btn.hide()
+
+        self.rotate_img_left.show()
+        self.rotate_img_right.show()
+        self.edit_img.show()
+
+    def brighten_options(self):
         pass
 
-    def delete_current_img(self):
+    def crop_options(self):
+        pass
+
+    def filter_options(self):
         pass
 
 
